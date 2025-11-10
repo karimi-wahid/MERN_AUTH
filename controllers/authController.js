2732a3ca484dce08a28766a8dc5b1ca42ced766d
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-11-10 07:04:32 +0430

    login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,3 +33,35 @@ export const register = async (req, res) => {
     }
 
 }
+
+export const login = async (req, res) => {
+    const {email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({success: false, message: 'All fields are required' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({success: false, message: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if(!isMatch) {
+            return res.status(400).json({success: false, message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', maxAge: 30 * 24 * 60 * 60 * 1000 });
+
+        res.status(200).json({success: true, message: 'Logged in successfully' });
+
+
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message });
+    }
+}
