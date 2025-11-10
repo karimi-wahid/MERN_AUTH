a9306e58141cd18892212a0150ff05e9154197c4
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-11-10 07:14:30 +0430

    authRoute api endpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,9 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+// API Endpoints
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
