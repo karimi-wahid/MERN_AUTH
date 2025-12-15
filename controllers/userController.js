7dfbe1454c6e5499b036dc0a42b25524b6c7e64a
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2025-12-15 14:34:47 +0430

    add user data retrieval endpoint and route

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+import User from "../models/userModel.js";
+
+export const getUserData = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      userData: {
+        name: user.name,
+        isAccountVerified: user.isAccountVerified,
+      },
+    });
+  } catch (error) {}
+};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import authRouter from './routes/authRoute.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,14 +12,15 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({ credentials: true }));
 
 // API Endpoints
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
-app.use('/api/auth', authRouter)
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
